d69cdedab45293288c20ebc539be272e3a582447
aac <andrey_europe_95hd@yahoo.com>
2020-09-29 16:09:10 +0200

    Add class transformer tags to UserDto

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,20 +1,53 @@
+import { Exclude, Expose } from 'class-transformer/decorators';
 import { MEDICAL_AID } from '../../medical_aids/models/medical_aid.model';
+@Exclude()
 export class UserDto {
+  @Expose()
   id: string;
+
+  @Expose()
   firstName: string;
+
+  @Expose()
   lastName: string;
+
+  @Expose()
   email: string;
+
+  @Expose()
   phone: string;
+
+  @Expose()
   avatarUrl?: string;
+
+  @Expose()
   countryCode: string;
+
+  @Expose()
   zipCode?: string;
+
+  @Expose()
   city?: string;
+
+  @Expose()
   address?: string;
+
+  @Expose()
   medicalAidNumber?: string;
+
+  @Expose()
   medicalAidPlan?: string;
+
+  @Expose()
   medicalAid?: MEDICAL_AID;
+
+  @Expose()
   isEmailVerified: boolean;
+
+  @Expose()
   isPhoneVerified: boolean;
+
+  @Expose()
   isActive: boolean;
 
   constructor(partial: Partial<UserDto>) {
