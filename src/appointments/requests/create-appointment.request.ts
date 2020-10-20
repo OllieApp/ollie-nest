e48e82062947f9670f0a71020dd3507a83e7a6d4
aac <andrey_europe_95hd@yahoo.com>
2020-10-20 15:44:04 +0200

    Add transformer for Date

--- src/appointments/requests/create-appointment.request.ts
+++ src/appointments/requests/create-appointment.request.ts
@@ -1,6 +1,14 @@
+import { Type } from 'class-transformer';
+import { IsDate } from 'class-validator/types/decorator/typechecker/IsDate';
+
 export class CreateAppointmentRequest {
   public practitionerId: string;
+
   public userNotes?: string;
+
+  @Type(() => Date)
+  @IsDate()
   public startTime: Date;
+
   public isVirtual: boolean;
 }
