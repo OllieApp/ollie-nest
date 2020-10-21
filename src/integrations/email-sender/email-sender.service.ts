d5ee23e519a273918b4785fa114956cf981022ab
aac <andrey_europe_95hd@yahoo.com>
2020-10-21 19:16:52 +0200

    Minor clean-up

--- src/integrations/email-sender/email-sender.service.ts
+++ src/integrations/email-sender/email-sender.service.ts
@@ -17,14 +17,4 @@ export class EmailSenderService {
       dynamicTemplateData: {},
     });
   }
-
-  async sendgridIntegrate() {
-    await this.sendgrid.send({
-      to: 'test@example.com', // Change to your recipient
-      from: 'test@example.com', // Change to your verified sender
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    });
-  }
 }
