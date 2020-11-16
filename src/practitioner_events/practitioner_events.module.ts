ee584e108d32dd88d8822e9acd904e5e52eb16fc
aac <andrey_europe_95hd@yahoo.com>
2020-11-16 16:22:04 +0100

    Add controller to module PractitionerEventsModule

--- src/practitioner_events/practitioner_events.module.ts
+++ src/practitioner_events/practitioner_events.module.ts
@@ -1,11 +1,18 @@
+import { PractitionersModule } from './../practitioners/practitioners.module';
+import { UsersModule } from './../users/users.module';
 import PractitionerEvent from 'src/practitioner_events/entities/practitioner_event.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PractitionerEventsService } from './practitioner_events.service';
+import { PractitionerEventsController } from './practitioner_events.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PractitionerEvent])],
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([PractitionerEvent]),
+    UsersModule,
+    PractitionersModule,
+  ],
+  controllers: [PractitionerEventsController],
   providers: [PractitionerEventsService],
   exports: [PractitionerEventsService],
 })
