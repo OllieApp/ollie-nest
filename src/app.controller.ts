ed954176883e9e6b85d47cc3edd99886c3012ac2
aac <andrey_europe_95hd@yahoo.com>
2020-12-03 18:30:18 +0100

    Add endpoint for app apple sign in

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, Res } from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -7,5 +7,14 @@ export class AppController {
   @Get('ping')
   pong(): string {
     return 'pong';
+  }
+
+  @Post('/app/callbacks/sign_in_with_apple')
+  fetchIntentCallback(@Request() request, @Res() response) {
+    const urlSearchParams = new URLSearchParams(request.body);
+    const redirectUri = `intent://callback?${urlSearchParams.toString()}#Intent;package=${
+      process.env.ANDROID_PACKAGE_IDENTIFIER
+    };scheme=signinwithapple;end`;
+    response.redirect(307, redirectUri);
   }
 }
