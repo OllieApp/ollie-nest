d9aa08d1576a6f7a1758f7f8709c30de18ac7957
aac <andrey_europe_95hd@yahoo.com>
2021-04-19 10:20:17 +0200

    Clean-up for practitioner dto

--- src/practitioners/dto/practitioner.dto.ts
+++ src/practitioners/dto/practitioner.dto.ts
@@ -3,27 +3,27 @@ import { PractitionerScheduleDto } from './practitioner-schedule.dto';
 import { MEDICAL_AID } from '../../medical_aids/models/medical_aid.model';
 import { PRACTITIONER_CATEGORY } from './category.dto';
 import { GENDER } from './gender.dto';
-import { Location } from './location.dto';
+import { AddressDto } from 'src/shared/dtos/address.dto';
+
 export class PractitionerDto {
-  id: string;
-  title: string;
-  email?: string;
-  phone?: string;
-  bio?: string;
-  description?: string;
-  address?: string;
-  appointmentTimeSlot: number;
-  consultationPricingRange: number;
-  medicalAids: MEDICAL_AID[];
-  category: PRACTITIONER_CATEGORY;
-  location?: Location;
-  isActive: boolean;
-  isVerified: boolean;
-  rating: number;
-  schedules: PractitionerScheduleDto[];
-  gender: GENDER;
-  languages: LANGUAGE[];
-  avatarUrl?: string;
+  public id: string;
+  public title: string;
+  public email?: string;
+  public phone?: string;
+  public bio?: string;
+  public address?: AddressDto;
+  public appointmentTimeSlot: number;
+  public consultationPricingRange: number;
+  public category: PRACTITIONER_CATEGORY;
+  public isActive: boolean;
+  public isVerified: boolean;
+  public isDisabled: boolean;
+  public rating: number;
+  public gender: GENDER;
+  public avatarUrl?: string;
+  public medicalAids: MEDICAL_AID[];
+  public schedules: PractitionerScheduleDto[];
+  public languages: LANGUAGE[];
 
   constructor(partial: Partial<PractitionerDto>) {
     Object.assign(this, partial);
